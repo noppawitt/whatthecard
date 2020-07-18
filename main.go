c2d8efb0a24b72ecdbb93c0180eb93b8544230d7
Noppawit Thairungroj <noppawit.trr@gmail.com>
2020-07-18 23:13:13 +0700

    Add port env

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"whatthecard/pkg/game"
@@ -11,11 +12,16 @@ import (
 func main() {
 	logLevel := os.Getenv("LOGLEVEL")
 	logger := logger.NewLogger(logLevel)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
 	hub := server.NewHub(logger)
 	gameService := game.NewService(logger)
 	server := server.New(hub, gameService, logger)
 
-	if err := server.Start(":4000"); err != nil {
+	if err := server.Start(fmt.Sprintf(":%s", port)); err != nil {
 		log.Fatal(err)
 	}
 }
